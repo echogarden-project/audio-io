15c5082dfba1caafaeb141cd5a2320e2eaadc346
Rotem Dan <rotemdan@gmail.com>
2024-11-02 08:00:20 +0200

    Simplify windows build

--- addons/windows-mme/build.cmd
+++ addons/windows-mme/build.cmd
@@ -1,5 +1,5 @@
 dlltool --dllname node.exe --def ../definitions/node_api.def --output-lib node_api.lib
 
-cl.exe windows-mme-output.cpp node_api.lib /I"../include/napi" /Fe"windows-x64-mme-output.node" /DNAPI_CPP_EXCEPTIONS /EHsc /std:c++20 /LD /O1 /link "winmm.lib"
+cl.exe windows-mme-output.cpp node_api.lib /I"../include/napi" /Fe"windows-x64-mme-output.node" /std:c++17 /EHsc /O1 /LD /link "winmm.lib"
 
 del *.obj *.exp windows-x64-mme-output.lib
